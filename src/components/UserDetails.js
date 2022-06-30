61c4f367ac5acbc95e209910613d546e01acb832
CubyckRubyk <oksanka.ukr14@gmail.com>
2022-06-30 11:01:20 +0300

    hm2part1

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-function UserDetails({User}) {
+function UserDetails({user}) {
     return (
         <div>
-<h2>{User.id}</h2>
-<h2>{User.name}</h2>
-<h2>{User.email}</h2>
-<div>{User.address.street}</div>
-<div>{User.address.suite}</div>
-<div>{User.address.city}</div>
-<div>{User.address.zipcode}</div>
-<div>{User.address.geo.lat}</div>
-<div>{User.address.geo.lng}</div>
+<h2>{user.id}</h2>
+<h2>{user.name}</h2>
+<h2>{user.email}</h2>
+<div>{user.address.street}</div>
+<div>{user.address.suite}</div>
+<div>{user.address.city}</div>
+<div>{user.address.zipcode}</div>
+<div>{user.address.geo.lat}</div>
+<div>{user.address.geo.lng}</div>
 </div>
     );
 }
