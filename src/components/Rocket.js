e7b2b0b92c902742fb2f14d8c83d97230f8eb427
CubyckRubyk <oksanka.ukr14@gmail.com>
2022-06-30 14:43:49 +0300

    hm2part2

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-function Rocket(props) {
+function Rocket({item: {mission_name, launch_year, links: {mission_patch_small}}}) {
     return (
-        <div></div>
+        <div>
+            <div>{mission_name}</div>
+            <div>{launch_year}</div>
+            <img src={mission_patch_small} alt={mission_name}/>
+        </div>
     );
 }
 
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,12 +1,18 @@
 import React, {useEffect, useState} from 'react';
+import {Rocket} from "./Rocket";
+
 
 function Rockets() {
-    const [rockets, setRockets] = useState();
+    const [rockets, setRockets] = useState([]);
     useEffect(() => {
         fetch('https://api.spacexdata.com/v3/launches/')
-    }))
+            .then(response => response.json())
+            .then(value => setRockets(value.filter(value => value.lanch_year !== '2020')))
+    }, []);
     return (
-        <div></div>
+        <div>
+            {rockets.map((rocket, index) => <Rocket key={index} item={rocket}/>)}
+        </div>
     );
 }
 
